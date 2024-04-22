9be1ad5ac217be59d189f93181c5e5049bad9bc4
ThiagoCorreiaBR2 <thiagogithub23@gmail.com>
2024-04-22 01:02:10 -0300

    Adição de som e imagem

--- src/App.js
+++ src/App.js
@@ -2,30 +2,27 @@ import React,{ useEffect, useState } from 'react';
 import './style.css';
 
 function App() {
+  const [pokemon, setPokemon] = useState({ weight: 0, height:0,sprites: {front_default: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png'},
+          cries:{latest: 'https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/25.ogg'}}); 
+  const [poke, setPoke] = useState();
 
-  const [pokemon, setPokemon] = useState({weight: 0, height: 0});
-  const [poke, setpoke] = useState();
-  
-  function loadAPI() {
-    let url = `https://pokeapi.co/api/v2/pokemon/${poke}`;    //pega o url
-    fetch(url)    //dropa no fetch
-    .then(response => response.json())    //larga oresultado do fetch na variavel response
-    .then(json=> {
+  async function loadAPI() {
+    try {
+      const url = `https://pokeapi.co/api/v2/pokemon/${poke}`;
+      const response = await fetch(url);
+      const json = await response.json();
+
       setPokemon(json);
-      console.log(json)
-    })
-
-    .catch(error => console.log(error))
+      console.log(json);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
   }
-  useEffect(() => {
-    loadAPI();
-  }, []);
 
 
-// Call the async function
-useEffect(() => {
-  loadAPI();
-}, []);
+
+  
+
 
 
   return (
@@ -36,17 +33,18 @@ useEffect(() => {
 
 
       <div>
-        <input type="text" onChange={(e) => setpoke(e.target.value)} />
+        <input type="text" onChange={(e) => setPoke(e.target.value)} />
         <button onClick={loadAPI}>Pesquisar</button>
         <div className='none'>
           <div>ID: {pokemon.id}</div>
           <div>Nome: {pokemon.name}</div>
           <div>Altura: {pokemon.height/10}</div>
           <div>Peso: {pokemon.weight/10}</div>
-          <div>
+          <div id='imagem'>
+            <img src={pokemon.sprites.front_default} alt={pokemon.name} />
           </div>
           <div >
-            
+            <audio autoPlay src={pokemon.cries.latest}></audio>
           </div>
         </div>
         
